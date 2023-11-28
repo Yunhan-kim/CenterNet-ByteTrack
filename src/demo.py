2bd13f97b96aec2b1b8d08adc2ee6d58e9f21037
Yunhan Kim <yunhan.kim91@gmail.com>
2023-11-28 18:15:51 +0900

    Fix: tracking demo

--- src/demo.py
+++ src/demo.py
@@ -24,14 +24,28 @@ def demo(opt):
     opt.demo[opt.demo.rfind('.') + 1:].lower() in video_ext:
     cam = cv2.VideoCapture(0 if opt.demo == 'webcam' else opt.demo)
     detector.pause = False
+
+    width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+    height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+    fps = cam.get(cv2.CAP_PROP_FPS)
+    save_folder = os.path.join('../output')
+    os.makedirs(save_folder, exist_ok=True)
+    save_path = os.path.join(save_folder, opt.demo.split("/")[-1])
+    vid.writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height)))
+
     while True:
         _, img = cam.read()
         cv2.imshow('input', img)
-        ret = detector.run(img)
+        if opt.tracking:
+          ret, online_im = detector.run(img)
+        else:
+          ret = detector.run(img)
         time_str = ''
         for stat in time_stats:
           time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
         print(time_str)
+        if opt.tracking:
+          vid_writer.write(online_im)
         if cv2.waitKey(1) == 27:
             return  # esc to quit
   else:
